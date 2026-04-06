012fa7593861d614fcbf0489153c44462347f528
Gaurav <gauravonlygaurav591@gmail.com>
2026-04-06 20:24:38 +0530

    Added Postman collection

--- src/main/java/com/example/financeData/controller/DashboardController.java
+++ src/main/java/com/example/financeData/controller/DashboardController.java
@@ -7,7 +7,6 @@ import com.example.financeData.services.FinancialRecordService;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
